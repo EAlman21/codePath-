b4a4061fd7067a702b08780b51442f5a89a85169
Macbook <Macbook@Enmanuels-MacBook.local>
2016-12-21 21:09:21 -0500

    took away all style tags and moved them to css

--- advancement/advance.php
+++ advancement/advance.php
@@ -11,29 +11,30 @@
     	<main class="mdl-layout__content">
     		<div class="mdl-card mdl-shadow--6dp">
     			<div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-    				<h2 style="margin-left:25%"class="mdl-card__title-text">Tip Calculator</h2>
+    				<h2 id="title"class="mdl-card__title-text">Tip Calculator</h2>
     			</div>
     	  	<div class="mdl-card__supporting-text">
     				<form action="#">
     					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    						<input style="width:40%; margin-left:30%;text-align:center;valign:middle;"class="mdl-textfield__input" type="text" name="bill"/>
-                <label style="text-align:center"class="mdl-textfield__label "for="bill">Bill Subtotal</label>
+    						<input id="input"class="mdl-textfield__input" type="text" name="bill"/>
+                <label id="label"class="mdl-textfield__label "for="bill">Bill Subtotal</label>
     					</div>
-              <p style="text-align:center">Tip Percentage</p>
-              <div style="margin-left:23%">
+              <p>Tip Percentage</p>
+              <div id="buttons">
                 <?php
-                  for ($i=10; $i <=20 ; $i = $i + 5) {
-                    echo "<label class=mdl-radio mdl-js-radio mdl-js-ripple-effect for=option-1>
-                           <input type=radio id=option-1 class=mdl-radio__button name=options value=.$i>
-                           <span class=mdl-radio__label>$i%</span>
-                          </label>";
+                  for ($i=10; $i <=20 ; $i = $i+5) {
+                    echo "
+                    <label class=mdl-radio mdl-js-radio mdl-js-ripple-effect for=option-1>
+                      <input type=radio id=option-1 class=mdl-radio__button name=options value=$i checked>
+                      <span class=mdl-radio__label>$i% </span>
+                    </label>";
                   }
-                 ?>
+                ?>
               </div>
     				</form>
     			</div>
     			<div class="mdl-card__actions mdl-card--border">
-    				<button type="submit" style="margin-left:35%"class="mdl-button mdl-button--colored mdl-js-button
+    				<button id="result" class="mdl-button mdl-button--colored mdl-js-button
              mdl-js-ripple-effect">Submit</button>
     			</div>
     		</div>
